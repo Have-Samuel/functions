c29fce008e88bee3122124138e1c835d2ade8a85
Have Samuel <samiehave9@gmail.com>
2022-11-17 02:48:38 +0300

    You can add functions into objects and you name them methods

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+// Direct Function
+function gameStart() {
+  console.log('Game is starting ---');
+}
+
+// const person = {
+//   greet: function greet() {
+//     console.log('Hello There!');
+//   }
+// };
+
+// person.greet();
+
+// Indirect Function
+startGameBtn.addEventListener('click', gameStart);
